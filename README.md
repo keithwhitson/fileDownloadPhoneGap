4df57b63768d63dd57b7f43be61f9015f4005ddb
keithwhitson <keith_whitson@yahoo.com>
2013-10-22 01:47:00 +0200

    first commit to test if everything is working

---
+++ README.md
@@ -0,0 +1,11 @@
+Online-Selling-App
+==================
+
+This will be anpp that helps online sellers to sell things easier and to keep up with things.  The idea is to have a photo and barcode of a product at the same time so you can keep track of everything.
+
+---------------------------
+The next step is to add the functionality for taking a picture with my camera.  
+
+This is no longer the app that it started out to be-
+
+Everything seems to be working now so I have turned the app into one that will be used to download an mp3 file into a smartphone-
---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+    // Wait for Cordova to connect with the device
+    //
+    document.addEventListener("deviceready",onDeviceReady,false);
+
+    // Cordova is ready to be used!
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT,0,gotFS,onFail);
+    }
+    
+    function gotFS(fileSystem){
+    	alert("filesystem successful");
+    	fileSystem.root.getFile("readme.txt",null,gotFileEntry, onFail);
+    }
+    
+    function gotFileEntry(fileEntry){
+    	alert("fileEntry was successful");
+    	fileEntry.file(gotFile,onFail);
+    }
+    
+    function gotFile(file){
+    	alert("gotFile successful");
+    	readDataUrl(file);
+    	readText(file);
+    }
+    
+    function readDataUrl(file){
+    	alert("readdateurl function is being called");
+    	var reader = new FileReader();
+    	reader.onloadend = function(evt){
+    		alert("Read as data URL");
+    		alert(evt.target.result);
+    	};
+    	
+    	reader.readAsDataURL(file);
+    }
+    
+    function readText(file){
+    	alert("readastext function is being called");
+    	var reader = new FileReader();
+    	reader.onloadend = function(evt){
+    		alert("Read as data Text");
+    		alert(evt.target.result);
+    	};
+    	
+    	reader.readAsText(file);
+    }
+	
+	// Called if something bad happens.
+    // 
+    function onFail(message) {
+      alert('Finding File Failed');
+    }
+    
+    function getFile(){
+    	alert("you want to get a file");
+   }
+
